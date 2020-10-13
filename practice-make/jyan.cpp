1ac47168ee35d933d8c83b52f50919e0388721ff
q12we34rt5 <61977113+q12we34rt5@users.noreply.github.com>
2020-10-13 13:40:51 +0800

    Update jyan.cpp

--- practice-make/jyan.cpp
+++ practice-make/jyan.cpp
@@ -1,5 +1,16 @@
 #include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
+#include<malloc.h>
+#include<algorithm>
+#include<list>
+#include<vector>
+#include<string>
+#include<queqe>
+#include<stack>
+#include<thread>
+#include<math.h>
 
 int main (){
-  printf ("Hellow World");
+  printf("Hellow World");
 }
